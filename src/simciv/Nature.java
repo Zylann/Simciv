bc9736a321fa20f99c598cfc0b94387ee8227121
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2012-07-08 15:43:17 +0200

    Added tree visual variants

--- src/simciv/Nature.java
+++ src/simciv/Nature.java
@@ -14,11 +14,13 @@ public class Nature
 	public static final byte NONE = 0;
 	public static final byte TREE = 1;
 	
-	private static Image tree;
+	private static Image treeSprites[] = new Image[3];
 	
 	public static void loadContent()
 	{
-		tree = ContentManager.instance().getImage("nature.tree");
+		treeSprites[0] = ContentManager.instance().getImage("nature.tree");
+		treeSprites[1] = ContentManager.instance().getImage("nature.tree2");
+		treeSprites[2] = ContentManager.instance().getImage("nature.tree3");
 	}
 	
 	/**
@@ -33,16 +35,23 @@ public class Nature
 		if(cell.nature == TREE)
 		{
 			// Apply a little offset to seem more "natural"
-			if((cell.noise & 0x01) != 0)
+			if((cell.noise & 0x01) != 0) // 0000 0001
 				gx -= 2;
-			else if((cell.noise & 0x02) != 0)
+			else if((cell.noise & 0x02) != 0) // 0000 0010
 				gx += 2;
-			else if((cell.noise & 0x03) != 0)
+			else if((cell.noise & 0x03) != 0) // 0000 0011
 				gy -= 2;
-			else if((cell.noise & 0x04) != 0)
+			else if((cell.noise & 0x04) != 0) // 0000 0100
 				gy += 2;
 			
-			gfx.drawImage(tree, gx, gy - (tree.getHeight() - Game.tilesSize));
+			// Visual variants
+			int i = 0;
+			if((cell.noise & 0x10) != 0) // 0001 0000
+				i = 1;
+			else if((cell.noise & 0x20) != 0) // 0010 000
+				i = 2;
+			
+			gfx.drawImage(treeSprites[i], gx, gy - (treeSprites[i].getHeight() - Game.tilesSize));
 		}
 	}
 }
