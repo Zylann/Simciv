497967e28ff1149184605ab892af35c3a68a4660
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2012-07-30 21:16:08 +0200

    Updated map generator (bush groups are better included with forests)

--- src/simciv/MapGenerator.java
+++ src/simciv/MapGenerator.java
@@ -29,31 +29,31 @@ public class MapGenerator extends Thread
     @Override
 	public void run() // Note : to execute the thread, use start().
 	{
-    	
     	generate(mapRef);
 	}
 
 	public void generate(Map map)
     {
     	int x, y;
-    	double n, n2, n3;
+    	double heightNoise, forestNoise, localNoise, bushNoise;
 		MapCell c = new MapCell();
 		
     	for(y = 0; y < map.getHeight(); y++)
     	{
     		for(x = 0; x < map.getWidth(); x++)
     		{
-    			n = Noise.getPerlin((float)x, (float)y, seed, 4, 0.5f, 64.f); // height noise
-    			n3 = Noise.getf(x, y, seed+1); // local noise
+    			heightNoise = Noise.getPerlin((float)x, (float)y, seed, 4, 0.5f, 64.f);
+    			localNoise = Noise.getf(x, y, seed+1);
     			
 				c.nature = 0;
-    			c.noise = (byte)(255.f*n3);
+    			c.noise = (byte)(255.f*localNoise);
 				
-    			if(n > 0.5)
+    			if(heightNoise > 0.5)
     			{
-        			n2 = Noise.getPerlin((float)x, (float)y, seed+1, 5, 0.5f, 24.f); // forest noise
+    				// Above sea-level
+        			forestNoise = Noise.getPerlin((float)x, (float)y, seed+1, 5, 0.5f, 24.f);
         			
-        			if(n < 0.6 && n2 > 0.5)
+        			if(heightNoise < 0.6 && forestNoise > 0.5)
         			{
         				// Beach
         				c.terrainID = Terrain.DUST;
@@ -61,44 +61,42 @@ public class MapGenerator extends Thread
         			else
         			{
         				// Land
-        				
         				c.terrainID = Terrain.GRASS;
         				
-        				if(n2 < 0.4)
+        				if(forestNoise < 0.4)
         				{
         					// Forest
-        					if(n3 < 0.7)
+        					if(localNoise < 0.7)
             					c.nature = Nature.TREE;
-        					else if(n3 < 0.85)
+        					else if(localNoise < 0.85)
         						c.nature = Nature.BUSH;
         				}
-        				else if(n2 < 0.45)
+        				else if(forestNoise < 0.45)
         				{
         					// Forest edge
-        					if(n3 < 0.1)
+        					if(localNoise < 0.1)
         						c.nature = Nature.TREE;
-        					else if(n3 < 0.2)
+        					else if(localNoise < 0.2)
         						c.nature = Nature.BUSH;
         				}
-        				else
-        				{
-        					// Bush group
-        					double n4 = Noise.getPerlin(x, y, seed+2, 5, 0.5f, 24.f);
-        					if(n4 < 0.3 && n3 < 0.9)
-        					{
-        						c.nature = Nature.BUSH;
-        						if(n3 < 0.88)
-        							c.noise &= 0x01;
-        					}
-        				}
+        				
+    					// Bush group
+    					bushNoise = Noise.getPerlin(x, y, seed+2, 5, 0.5f, 24.f);
+    					if(bushNoise < 0.3 && localNoise < 0.9 && c.nature == Nature.NONE)
+    					{
+    						c.nature = Nature.BUSH;
+    						if(localNoise < 0.88)
+    							c.noise &= 0x01;
+    					}
            			}        			
     			}
     			else
-    				c.terrainID = Terrain.WATER; // ocean
-    			    			
+    				c.terrainID = Terrain.WATER; // sea
+    			    			    			
     			map.getCellExisting(x, y).set(c);
     		}
     		
+    		// Update generation progress
     		progress = (int) (100 * (float)y / (float)map.getHeight());
     	}
     	
